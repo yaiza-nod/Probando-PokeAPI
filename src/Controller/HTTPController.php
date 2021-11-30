016679e9644e45102c58347a0ce685bcb6392399
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-11-30 12:14:50 +0100

    HTTPController now works and fetches pokemon info from PokeAPI

--- src/Controller/HTTPController.php
+++ src/Controller/HTTPController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,13 +9,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HTTPController extends AbstractController
 {
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     /**
-     * @Route("/h/t/t/p", name="h_t_t_p")
+     * @Route("/pokeapi", name="pokeapi")
      */
     public function index(): Response
     {
-        return $this->render('http/index.html.twig', [
+        $pokemon = $this->fetchPokemon();
+
+        echo var_dump($pokemon);
+
+        return $this->render('http/httpController.html.twig', [
             'controller_name' => 'HTTPController',
         ]);
+    }
+
+    public function fetchPokemon(): array
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://pokeapi.co/api/v2/pokemon/ditto'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content;
     }
 }
