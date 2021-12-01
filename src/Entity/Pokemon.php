4e6c24848d817b0e1798d1f4005b4a583c0ee881
Yaiza Fernández <yaiza.fernandez@nodrizatech.com>
2021-12-01 11:15:41 +0100

    Add Pokemon class

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PokemonRepository::class)
+ */
+class Pokemon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $abilities = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAbilities(): ?array
+    {
+        return $this->abilities;
+    }
+
+    public function setAbilities(?array $abilities): self
+    {
+        $this->abilities = $abilities;
+
+        return $this;
+    }
+}
